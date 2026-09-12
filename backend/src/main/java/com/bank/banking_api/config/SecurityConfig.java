36ce5582308aa35aecc56068d78e817123d395d2
sandeepp712 <sandeepprajapat200@gmail.com>
2026-09-12 08:48:00 +0000

    fix the small cors origin

--- backend/src/main/java/com/bank/banking_api/config/SecurityConfig.java
+++ backend/src/main/java/com/bank/banking_api/config/SecurityConfig.java
@@ -61,9 +61,9 @@ public class SecurityConfig {
         // 1. Explicitly list th frontend URL (local +Production)
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:5173",
-                "https://shinro-bank.duckdns.org",
+//                "https://shinro-bank.duckdns.org",
                 "https://d9qweidhuephf.cloudfront.net"
-        ));
+                ));
 
         // Allowed HTTP methods
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
